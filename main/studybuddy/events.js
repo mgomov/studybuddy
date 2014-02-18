62cd930f25bbf4328863681eaa1ff366f83a491f
Maxim Gomov <maximgomov@gmail.com>
2014-02-18 11:39:08 -0800

    Restore?
    Restoring from my local copy

---
+++ main/studybuddy/events.js
@@ -0,0 +1,39 @@
+
+// -1 is before the first annotation
+// -2 is after the last annotation
+function update_event(current_event, events, current_time){
+	if(events[events.length - 1].time + events[events.length - 1].duration < current_time){
+		return -2;
+		changed = true;
+	}
+	
+	if(events[0].time > current_time){
+		return -1;
+		changed = true;
+	}
+	
+	for(var i = 0; i < events.length; i++){
+		if(events[i].time <= current_time && events[i].time + events[i].duration >= current_time){
+			if(i != current_event){
+				changed = true;
+			}
+			return i;
+		}
+	}
+	return -3;
+}
+
+// TODO
+function add_annotation(time, duration, text, image, events){
+	
+}
+
+// TODO
+function edit_annotation(time, duration, text, image, event_number){
+
+}
+
+// TODO
+function remove_annotation(event_number){
+
+}
