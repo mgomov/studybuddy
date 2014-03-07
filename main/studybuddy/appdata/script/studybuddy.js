c39deda5a1bfb8ddf431b5546cb7c5b0fd34af61
Maxim Gomov <maximgomov@gmail.com>
2014-03-07 11:14:41 -0800

    Doxygen commit
    Possibly not permanent

--- main/studybuddy/appdata/script/studybuddy.js
+++ main/studybuddy/appdata/script/studybuddy.js
@@ -2,77 +2,56 @@
  * Ties various things together... Global variables go here
  */
 
-// Width and height, for general use
-var w = window.innerWidth;
-var h = window.innerHeight;
+var w = window.innerWidth;	///< The height of the window that we can work with
+var h = window.innerHeight; ///< The width of the window we can work with
 
-// Reference to the master file so we can write back to it later
-var master_file_reference;
+var master; ///< Reference to the master file(main.sb) for accessing the recordings
 
-// Main file that holds all the info
-var master;
-// The current recording file's parsed JSON
-var recording;
-// Recording file's name
-var recording_file;
-// Current event (integer, index for recording.Events)
-var current_event;
+var recording; ///< Reference to the current recording that's loaded up
 
-// Audio player
-var audio = document.getElementById("audio_player");
+var current_event; ///< The current event from the recording that is being displayed now (with annotations, etc)
 
-// The main image canvas
-var image_canvas = document.getElementById("image_canvas");
-var image_context = image_canvas.getContext("2d");
+var audio = document.getElementById("audio_player");  ///< The audio player which handles our audio
 
-// Image up for display currently
-var main_image = document.getElementById("main_image");
+var image_canvas = document.getElementById("image_canvas");  ///< Image canvas that takes up the whole screen
+var image_context = image_canvas.getContext("2d"); ///< Image cavnas' context, for drawing to
 
-// Convenience vars for switching between events... 
-// Signals that an event was switched and some things need to happen
-var event_switched = false;
-// Signals that the image was fully loaded, done to prevent 'flashbacks' of
-// previous image
-var image_updated = false;
+var event_switched = false; ///< Boolean which tells if the event was switched and things need to be changed
 
-// The seek bar's canvas
-var seek_canvas = document.getElementById("seek_canvas");
-var seek_context = seek_canvas.getContext("2d");
+var image_updated = false;  ///< Irrelevant now, needs to be removed from our current scheme
 
-// The seek bar's overlay canvas
-var seek_overlay_canvas = document.getElementById("seek_overlay_canvas");
-var seek_overlay_context = seek_overlay_canvas.getContext("2d");
+var seek_canvas = document.getElementById("seek_canvas");  ///< The seek bar's discrete canvas that we draw all of the seek bar things to
+var seek_context = seek_canvas.getContext("2d"); ///< Seek bar's context
 
-// Annotation stream
-var annotation_stream = document.getElementById("annotation_stream");
-// Input box
-var annotation_input = document.getElementById("annotation_input");
+var seek_overlay_canvas = document.getElementById("seek_overlay_canvas");  ///< Seek bar's overlay, where we draw things like the current time we're at and where images are
+var seek_overlay_context = seek_overlay_canvas.getContext("2d");  ///< Seek overlay's context
 
-var play_pause_time = 0;
+var play_pause_time = 0;  ///< Var for controlling transparency for fading of the play/pause indicators
 
-var drag = false;
-var delta = 0;
-var justdragged = false;
-var moving_point;
+var drag = false;  ///< Tells us if we're currently dragging a window... For input
+var delta = 0;  ///< Delta of the current drag
+var justdragged = false;  ///< Used for preventing click event after a drag
+var moving_point;  ///< The point we're dragging around the screen
 
-var editing_point = false;
+var editing_point = false;  ///< Boolean that tells us if a variable is being edited
 
-// Block for context menu for points (right click menu)
-var point_ctx_display = false;
-var point_ctx = document.getElementById("point_context_menu");
+var point_ctx_display = false;  ///< Boolean telling us if we're displaying an editing menu for points
+var point_ctx = document.getElementById("point_context_menu"); ///< The editing menu for all points
 
-// Temp var to pass files to main merge, DO NOT REFERENCE outside of merge process
-var _TEMPFILES;
+var _TEMPFILES;  ///< Temp var to pass files to main merge, DO NOT REFERENCE outside of merge process
 
-var images = new Array();
-var seek_draw = false;
-var seek_draw_event;
-var seek_draw_x;
+var images = new Array();  ///< Array of images for the current recording
+var seek_draw = false;  ///< Boolean telling us if we should be drawing the mouseover preview
+var seek_draw_event;  ///< The number of the event that we're previewing (in context of Recording[seek_draw_event])
+var seek_draw_x;  ///< What we're mousing over in terms of x coordinates
 
-var seek_display = false;
-var seek_display_opacity = 1.0;
+var seek_display = false; ///< Boolean that tells us if we're moused over the seek bar or not
 
-// Init function; called when window is resized as well as on startup
+var seek_display_opacity = 1.0; ///< Var for controlling transparency for fading of the seek bar
+/*! \fn 	init
+ * 	\brief 	Initializes the window width and heights properly.
+ *  \brief  Called on startup and window resize.
+ */
 function init(){
 	w = window.innerWidth;
 	h = window.innerHeight;
@@ -90,7 +69,7 @@ function init(){
 	seek_canvas.relativeY = image_canvas.height - seek_canvas.height - 50;
 }
 
-init();
+init(); 
 render_main_screen();
 
 // Adds render of the main canvas to a loop
@@ -105,7 +84,13 @@ window.addEventListener('resize', function(event){
 	init();
 });
 
-// Volume adjuster
-function change_volume(slider_vol){
+
+/*! \fn 	change_volume
+ * 	\brief 	Changes the volume
+ *  \brief  Called from the DOM element "vol_slider"
+ * 	\var
+ */
+
+function change_volume(slider_vol/*!< Param for the volume */){
 	audio.volume = slider_vol / 100;
 }
